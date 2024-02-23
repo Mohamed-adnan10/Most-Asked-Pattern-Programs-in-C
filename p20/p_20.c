130f9d95ad2e14314f9a38803af5b0da68d4b59b
unknown <moadnan919@gmail.com>
2024-02-23 21:21:44 +0200

    add p20

---
+++ p20/p_20.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main (void)
+{
+    unsigned int size;
+    unsigned int count1;
+    unsigned int count2;
+    unsigned int count3;
+
+    printf("enter number of size :");
+    scanf("%d" ,&size);
+
+    for(count1 = 1; count1 <= size; count1++)
+    {
+        for (count2 = size; count2 >= count1; count2--)
+        {
+                printf("  ");
+        }
+        for (count3 = 1; count3 <= count1; count3++)
+        {
+                printf("*   ");
+        
+        }
+        printf("\n");
+    }
+
+    for(count1 = 1; count1 <= size; count1++)
+    {
+        for (count2 = 1; count2 < count1; count2++)
+        {
+                printf("  ");
+            
+        }
+        for (count3 = size; count3 >= count1; count3--)
+        {
+                printf("  * ");
+        
+        }
+        printf("\n");
+        
+    }
+    
+
+    return 0;
+}
+
+/*
+
+enter number of size :5
+          *   
+        *   *
+      *   *   *
+    *   *   *   *
+  *   *   *   *   *
+  *   *   *   *   *
+    *   *   *   *
+      *   *   *
+        *   *
+          * 
+
+*/
